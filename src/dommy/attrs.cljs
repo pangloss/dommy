f12ff525152ab81494c879a20d3ea98f10d5749a
Darrick Wiebe <dw@xnlogic.com>
2014-02-05 14:38:06 -0500

    Handle case of element having no classes

--- src/dommy/attrs.cljs
+++ src/dommy/attrs.cljs
@@ -52,7 +52,7 @@
            (doseq [class (.split classes #"\s+")]
              (.add class-list class))
            (doseq [class (.split classes #"\s+")]
-             (let [class-name (.-className elem)]
+             (let [class-name (or (.-className elem) "")]
                (when-not (class-index class-name class)
                  (set! (.-className elem)
                        (if (identical? class-name "")
